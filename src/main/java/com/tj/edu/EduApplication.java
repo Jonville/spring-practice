6e6e193bcf79d84082db55a73c0cd4c2567ad63d
Jonville <126869858+Jonville@users.noreply.github.com>
2023-07-05 15:09:17 +0900

    Revert "1. practice2 관련소스" (#2)

--- src/main/java/com/tj/edu/EduApplication.java
+++
@@ -1,9 +0,0 @@
-package com.tj.edu;
-
-import com.fasterxml.jackson.databind.jsonFormatVisitors.JsonAnyFormatVisitor;
-
-public class EduApplication {
-    public static void main(String[] args) {
-
-    }
-}
--- src/main/java/com/tj/edu/practice2/spring_ioc/IEncoder.java
+++
@@ -1,9 +0,0 @@
-package com.tj.edu.practice2.spring_ioc;
-
-public interface IEncoder {
-
-    String encodeString(String message);
-
-    byte[] encodeByte(String message);
-
-}
